cbd5b387f882d3cb1198b1470e773b116c674232
ibaldo <43021939+igbaldo@users.noreply.github.com>
2018-11-19 23:02:33 -0300

    Revert "Eliminacion de carpetas inservibles, modificacion de custom.css y creacion en mvc de menu y menu-itmes"
    This reverts commit 3e1297e5e4357de6048a76327aa943f91f6a4bfa.

--- ABM- MENU-ITEMS/Menu-items-modificar.php
+++ ABM- MENU-ITEMS/Menu-items-modificar.php
@@ -1,4 +1,6 @@
-
+<?php
+        $conn = mysqli_connect("127.0.0.1","root","","specialfooddb");
+?>
 <!DOCTYPE html>
 <html lang="es" class="no-js">
 <head>
@@ -21,16 +23,16 @@
 			<!--
 			CSS
 			============================================= -->
-			<link rel="stylesheet" href="/css/linearicons.css">
-      <link rel="stylesheet" href="/css/font-awesome.min.css">
-      <link rel="stylesheet" href="/css/bootstrap.css">
-      <link rel="stylesheet" href="/css/magnific-popup.css">
-      <link rel="stylesheet" href="/css/jquery-ui.css">       
-      <link rel="stylesheet" href="/css/nice-select.css">             
-      <link rel="stylesheet" href="/css/animate.min.css">
-      <link rel="stylesheet" href="/css/owl.carousel.css">        
-      <link rel="stylesheet" href="/css/main.css">
-      <link rel="stylesheet" href="/css/custom.css">
+			<link rel="stylesheet" href="../web/css/linearicons.css">
+      <link rel="stylesheet" href="../web/css/font-awesome.min.css">
+      <link rel="stylesheet" href="../web/css/bootstrap.css">
+      <link rel="stylesheet" href="../web/css/magnific-popup.css">
+      <link rel="stylesheet" href="../web/css/jquery-ui.css">       
+      <link rel="stylesheet" href="../web/css/nice-select.css">             
+      <link rel="stylesheet" href="../web/css/animate.min.css">
+      <link rel="stylesheet" href="../web/css/owl.carousel.css">        
+      <link rel="stylesheet" href="../web/css/main.css">
+      <link rel="stylesheet" href="../web/css/custom.css">
        </head>
        <body>	
         <header id="header" class="absolute">
@@ -38,7 +40,7 @@
             <div class="row">			
                 <nav id="nav-menu">
                     <ul class="nav-menu ">
-                        <li class="volver"><a href="/"> << Volver</a></li>
+                        <li class="volver"><a href="../web/index.html"> << Volver</a></li>
                     </ul>
                 </nav>				      		  
             </div>
@@ -47,7 +49,7 @@
             <div class="container">
                 <div class="row justify-content-center">
                     <div id="logo">
-                     <a href="/"><img src="/img/logo.png" alt="" title="" /></a>
+                     <a href="../web/index.html"><img src="../web/img/logo.png" alt="" title="" /></a>
                  </div>
              </div>			  					
          </div>
@@ -65,7 +67,7 @@
                         
                     </div>
                     <div class="panel-body">
-                        <form role="form" method="POST" action="/menuitems/guardar">  
+                        <form role="form" method="POST" action="Menu-items-nuevo.php">  
                     
                         <br>
                         <h3 class="letra-blanca">Producto</h3>
@@ -98,7 +100,47 @@
                 </div>
 
             </div>
-       
+            <?php
+            if(isset($_POST["guardar"])){
+              
+              if($_POST["Producto_nombre"]==null || $_POST["Producto_precio"]==null || $_POST["imagen"]==null){
+               echo "<br>"."<div class='letraerror'>"."No se aceptan campos vacios"."</div>";
+                }
+
+                elseif (!(is_numeric($_POST['Producto_precio']))) {
+                 echo "<br>"."<div class='letraerror'>"."El Precio es un capo numerico"."</div>";
+                }
+                else{
+                $producto=$_POST['Producto_nombre'];
+                $producto= strtolower($producto);
+                $producto= ucfirst($producto);
+
+                $precio=$_POST['Producto_precio'];
+
+                $imagen=$_POST['imagen'];
+
+
+                $sql = "select mni.Descripcion d
+                from MenuNegocioItem mni 
+                where mni.Descripcion='$producto'";
+
+                $result=mysqli_query($conn,$sql);
+                $asd=mysqli_fetch_assoc($result);
+
+                if(!($asd['d']==$producto)){
+
+                  $c=1;
+                  $sql2="Insert Into MenuNegocioItem (Descripcion,Precio, Foto, IdMenuComercio) values('$producto','$precio','imagen','$c')";        
+                  $result2=mysqli_query($conn,$sql2);
+
+                  echo "<br>"."<div class='letraerror'>"."Guardado Correctamente"."</div>";
+                }
+                else{
+                  echo "<br>"."<div class='letraerror'>"."Producto Existente"."</div>";
+                } 
+              }  
+              }
+              ?>
         </div>
     </div>
 </div>
@@ -106,11 +148,11 @@
 </section>
 <footer class="footer-area">
    <?php
-      include 'footer.php';
+  include'../web/footer.php';
     ?>
 </footer>
 <script
 src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="/js/validar-formulario.js"></script>           
+<script src="../web/js/validar-formulario.js"></script>           
 </body>
 </html>
--- SpecialFoodMVC/application/controller/controller_menuitems.php
+++ SpecialFoodMVC/application/controller/controller_menuitems.php
@@ -18,16 +18,14 @@ class Controller_menuitems extends Controller{
         $precio=$_POST['Producto_precio'];
 
         $imagen=$_POST['imagen'];
-        $idMenu=$_POST['idMenu'];
 
         if (empty($producto) || empty($precio)|| empty($imagen)) {
 
-            echo "<br>"."<div class='letraerror'>"."Campos vacios"."</div>";
+            echo "<br>"."<div class='letraerror'>"."El Precio es un capo numerico"."</div>";
 
         } else {
 
-            $guardar = $this->model->guardar($producto , $precio, $imagen,$idMenu);
-
+            $guardar = $this->model->guardar($producto , $precio, $imagen);
 
             if($guardar == true){
             	echo "<br>"."<div class='letraerror'>"."Guardado Correctamente"."</div>";
@@ -37,37 +35,4 @@ class Controller_menuitems extends Controller{
             }
         }
     }
-
-    function Eliminar(){
-        $id = $_GET["id"];
-        $data = $this->model->getListParaEliminar($id);
-        $this->view->generate('listarmenu.php', 'template_view.php', $data);
-    }
-
-    function Modificar (){
-        $id = $_GET["id"];
-        $data = $this->model->getListParaModificar($id);
-        $this->view->generate('ModificarMenuItems.php', 'template_view.php', $data);
-    }
-
-    function actualizar(){
-
-
-        $Descripcion=$_POST['Producto_nombre'];
-        $Descripcion= strtolower($Descripcion);
-        $Descripcion= ucfirst($Descripcion);
-        $Precio=$_POST['Producto_precio'];
-        $Imagen=$_POST['imagen'];
-        $id=$_POST['idMenu'];
-
-        if($Descripcion==null || $Precio==null ) {
-            echo "<br>"."<div class='letraerror'>"."No se aceptan campos vacios"."</div>";
-        }
-        else{
-
-            $data=$this->model->actualizar($Descripcion,$Precio,$Imagen,$id);
-            $this->view->generate('listarmenu.php', 'template_view.php', $data);
-        }
-
-}
 }
